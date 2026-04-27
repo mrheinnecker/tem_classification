c1d6cace15e1c1866b8f717f481af04bef5c2f03
Marco Rheinnecker <marco.rheinnecker@gmail.com>
2026-04-27 15:10:42 +0200

    changed colors to colorblind safe version

--- scripts_marco/count_stats.R
+++ scripts_marco/count_stats.R
@@ -12,7 +12,7 @@
 
 make_main_statistic_of_sample_number <- function(raw_dir){
   
-  all_sites <- c("TAL","KRI","BIL","POR","BAR","NAP","ATH", "VIG")
+  all_sites <- c("TAL","KRI","BIL","POR","BAR","NAP","ATH") #%>% c(., "VIG")
   all_files_raw <- 
     tibble(file=list.files(raw_dir, pattern="c\\d+.mrc$", recursive = T, full.names=T) %>%
              .[which(!str_detect(.,"canc_"))]) %>%
@@ -32,16 +32,32 @@ make_main_statistic_of_sample_number <- function(raw_dir){
   cnts <- all_files_raw  %>%
     group_by(site, .drop=F)  %>%
     tally() %>%
-    mutate(frac=n/200)
+    mutate(frac=n/200,
+           fac="left") #%>%
+    # bind_rows(
+    #   tibble(
+    #     fac="right",
+    #     n=sum(.$n),
+    #     frac=1,
+    #     site="all"
+    #   )
+    # )
+  
+  col_high <- "#0C7BDC"
+  col_low <- "#FFC20A"
   
   
-  barplot <- ggplot(cnts,
+  barplot <- 
+    ggplot(cnts,
                     aes(x=site, y=n, fill=frac))+
     geom_col()+
+    #facet_grid("1"~fac, scales="free", space="free")+
     geom_text(aes(label=n, y=n-2), vjust=1, color="white")+
-    scale_fill_gradientn(colors=c("orange", "forestgreen"), limits=c(0,1), 
+    scale_fill_gradientn(colors=c("grey",col_low,col_low,  col_high,col_high), limits=c(0,1), 
                          breaks=seq(0,1,0.25), labels=200*seq(0,1,0.25),
                          name="progress")+
+    geom_label(data=tibble(x="ATH", y=150, label=paste0("total: ", as.character(sum(cnts$n)))),
+              aes(x=x, y=y, label=label), inherit.aes=F)+
     theme_bw()+
     ylab("cells imaged")
   
@@ -52,7 +68,7 @@ make_main_statistic_of_sample_number <- function(raw_dir){
     ggplot(data=., aes(x=site, y=size_frac, fill=n))+
     geom_tile()+
     geom_text(aes(label=n), color="white")+
-    scale_fill_gradientn(colors=c("grey","blue"), limits=c(0,200))+
+    scale_fill_gradientn(colors=c("grey",col_low,col_low,  col_high,col_high), limits=c(0,200))+
     theme_bw()+
     ylab("size fraction")
   
@@ -64,7 +80,7 @@ make_main_statistic_of_sample_number <- function(raw_dir){
     ggplot(data=., aes(x=site, y=sampling_time, fill=n))+
     geom_tile()+
     geom_text(aes(label=n), color="white")+
-    scale_fill_gradientn(colors=c("grey","blue"), limits=c(0,200))+
+    scale_fill_gradientn(colors=c("grey",col_low,col_low,  col_high,col_high), limits=c(0,200))+
     theme_bw()+
     ylab("daytime")
   
@@ -74,10 +90,14 @@ make_main_statistic_of_sample_number <- function(raw_dir){
       plotlist=c(barplot+theme(axis.title.x = element_blank(), 
                                axis.text.x = element_blank(), 
                                axis.ticks.x = element_blank(),
-                               legend.position="none"), 
+                              # legend.position="none"
+                               ), 
                  sampling_time_plot+theme(axis.title.x = element_blank(), axis.text.x = element_blank(), axis.ticks.x = element_blank(),
-                                          legend.position="none"), 
-                 sizefrac_plot+theme(legend.position="none")), 
+                                          legend.position="none"
+                                          ), 
+                 sizefrac_plot+theme(
+                   legend.position="none"
+                   )), 
       nrow=3,
       align="v",
       rel_heights = c(2,1, 1.1)
