4b033216eb31e8ee7947a121fa00deb76cce94cb
Marco Rheinnecker <marco.rheinnecker@gmail.com>
2026-05-16 07:49:41 +0000

    updated main executor script

--- scripts_marco/main.sh
+++ scripts_marco/main.sh
@@ -1,11 +1,12 @@
 #!bin/bash
 
 workflow_dir="$1"
-#workflow_dir="/g/schwab/marco/repos/tem_classification/scripts_marco"
+
+workflow_dir="/g/schwab/marco/repos/tem_classification/scripts_marco"
 
 container_mrcfile="/g/schwab/marco/container_legacy/py_mrcfile.sif"
 container_imod="/g/schwab/marco/container_legacy/EMBL_IMOD_5.1.0-foss-2023a-CUDA-12.1.1.sif"
-container_tidyverse="/g/schwab/marco/container_legacy/probeDesign_rtool.sif"
+container_tidyverse="/g/schwab/marco/container_devel/probeDesign_rtool.sif"
 
 timestamp=$(date +%Y-%m-%d_%H-%M)
 
@@ -36,11 +37,13 @@ nextflow run "${workflow_dir}/wfTEM.nf" \
 
 
 
-
-
-
-
-
+   eubi to_zarr \
+      /scratch/rheinnec/245756_S2_Cut2_c019_116114649_KRI_10to40_20230802_PM_01_epo_02_P1/245756_S2_Cut2_c019_116114649_KRI_10to40_20230802_PM_01_epo_02_P1_correctionblend.mrc \
+      /scratch/rheinnec/alex_omezarr/245756_S2_Cut2_c019_116114649_KRI_10to40_20230802_PM_01_epo_02_P1_correctionblend_omezarr \
+      --x_unit nm \
+      --y_unit nm \
+      --x_scale 1.766 \
+      --y_scale 1.766
 
 
 
