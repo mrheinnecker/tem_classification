4eab08b8bd74455abea24c95a1dea1df31ba77f5
Marco Rheinnecker <marco.rheinnecker@gmail.com>
2026-08-19 16:38:36 +0000

    geenralized to also process other cryo datalayer

--- cryo/cryo_ov_figure.R
+++ cryo/cryo_ov_figure.R
@@ -8,22 +8,20 @@ library(tidyverse)
 library(ggalluvial)
 library(googlesheets4)
 
-
-response_colors = tribble(
+gs4_auth()
+rc_hpf = tribble(
   ~response,          ~col,   ~lab,
   
   # Baltic / North Sea — cool
-  "Kristineberg",     "#332288", "Kristineberg", # deep indigo   
+  "Roscoff",          "#332288",   "Roscoff",# deep indigo   "#332288"
   "Tallinn",          "#4477AA",  "Tallinn",   # blue
-  "Roscoff",          "#66CCEE",   "Roscoff", # cyan
+  "Kristineberg",     "#66CCEE", "Kristineberg",
   "Bilbao",           "#228833",  "Bilbao",   # green
   "Porto",            "#AA9933",   "Porto",  # ochre
   "Barcelona",        "#EE7733",   "Barcelona",# orange
   "Naples",           "#CC6677",  "Naples",   # coral / rose
   "Athens",           "#AA3377",   "Athens",    # magenta
-  
   # Other categories
- 
   "fully covered",    "grey10",   "fully\ncovered",
   "partly covered",   "grey40",  "partly\ncovered",  
   "empty planchette", "grey70", "empty\nplanchette",
@@ -37,23 +35,104 @@ response_colors = tribble(
  
 )
 
-survey_colors=tibble(
+
+rc_pf = tribble(
+  ~response,          ~col,   ~lab,
+  
+  # Baltic / North Sea — cool
+  "Roscoff",          "#332288",   "Roscoff",# deep indigo   "#332288"
+  "Tallinn",          "#4477AA",  "Tallinn",   # blue
+  "Kristineberg",     "#66CCEE", "Kristineberg",
+  "Bilbao",           "#228833",  "Bilbao",   # green
+  "Porto",            "#AA9933",   "Porto",  # ochre
+  "Barcelona",        "#EE7733",   "Barcelona",# orange
+  "Naples",           "#CC6677",  "Naples",   # coral / rose
+  "Athens",           "#AA3377",   "Athens",    # magenta
+  
+  "good1",        "grey10"  ,  "good",      
+  "medium1",           "grey40",   "medium",  
+  "bad1",         "grey70",  "bad",  
+  
+  "good2",        "grey10"  ,  "good",
+  "medium2",           "grey40",   "medium",
+  "bad2",         "grey70",  "bad",
+  
+  
+)
+
+rc_wm <- rc_pf
+
+sc_hpf=tibble(
   survey=c("Site", "eye_test_freezing_quality", "amount_of_signal" ),
   lab=c("Site", "Freezing quality", "Signal intensity"),
   top_lab_raw=c("cryo HPF samples", "visually inspected", "imaged")
 )
 
+sc_pf=tibble(
+  survey=c("Site", "Freezing_quality", "Sample_quality" ),
+  lab=c("Site", "Freezing quality", "Sample quality"),
+  top_lab_raw=c("cryo PF samples", "visually inspected", "imaged")
+)
 
-frido_tab <- "https://docs.google.com/spreadsheets/d/1Ln2YVfxMtQNc-eSh69RA0tpc5UhsMNFXizfCVAR4iZI/edit?gid=0#gid=0"
+sc_wm=tibble(
+  survey=c("Site", "Freezing_quality", "Sample_quality" ),
+  lab=c("Site", "Freezing quality", "Sample quality"),
+  top_lab_raw=c("cryo WM samples", "visually inspected", "imaged")
+)
 
 
-raw_df <- read_sheet(frido_tab,
-                   col_types="c")
+
+
+hpf_tab <- "https://docs.google.com/spreadsheets/d/1Ln2YVfxMtQNc-eSh69RA0tpc5UhsMNFXizfCVAR4iZI/edit?gid=0#gid=0"
+wm_tab="https://docs.google.com/spreadsheets/d/1Ln2YVfxMtQNc-eSh69RA0tpc5UhsMNFXizfCVAR4iZI/edit?gid=0#gid=0"
+pf_tab="https://docs.google.com/spreadsheets/d/1Ln2YVfxMtQNc-eSh69RA0tpc5UhsMNFXizfCVAR4iZI/edit?gid=0#gid=0"
 
 
 
 
-df <- raw_df %>%
+raw_hpf <- read_sheet(hpf_tab,
+                   col_types="c", sheet="hpf")
+
+
+raw_pf <- read_sheet(pf_tab,
+                      col_types="c", sheet="pf")
+
+
+raw_wm <- read_sheet(pf_tab,
+                     col_types="c", sheet="wm")
+
+
+df_pf <- raw_pf %>%
+  #filter(!is.na(Freezing_quality))%>%
+  select(Site, Freezing_quality, Sample_quality) %>%
+  rownames_to_column("id") %>%
+  pivot_longer(cols=names(.)[which(names(.)!="id")], names_to = "survey", values_to = "response")  %>%
+  mutate(
+    response=case_when(
+      #is.na(reponse) ~ response,
+      survey=="Freezing_quality" ~ paste0(response, 1),
+      survey=="Sample_quality" ~ paste0(response, 2),
+      TRUE ~ response
+    ),
+    response=ifelse(response %in% c("NA1", "NA2"), NA, response)
+  )
+  
+df_wm <- raw_wm %>%
+  #filter(!is.na(Freezing_quality))%>%
+  select(Site, Freezing_quality, Sample_quality) %>%
+  rownames_to_column("id") %>%
+  pivot_longer(cols=names(.)[which(names(.)!="id")], names_to = "survey", values_to = "response") %>%
+  mutate(
+    response=case_when(
+      #is.na(reponse) ~ response,
+      survey=="Freezing_quality" ~ paste0(response, 1),
+      survey=="Sample_quality" ~ paste0(response, 2),
+      TRUE ~ response
+    ),
+    response=ifelse(response %in% c("NA1", "NA2"), NA, response)
+  )
+
+df_hpf <- raw_hpf %>%
   filter(is.na(block_name_if_produced))%>%
   mutate(imaged=ifelse(is.na(Imaged), F, Imaged)) %>%
   select(Site, eye_test_freezing_quality, 
@@ -62,6 +141,47 @@ df <- raw_df %>%
   rownames_to_column("id") %>%
   pivot_longer(cols=names(.)[which(names(.)!="id")], names_to = "survey", values_to = "response") 
 
+
+
+
+make_cryo_alluvial_figure(df=df_hpf, 
+                          response_colors=rc_hpf, 
+                          survey_colors=sc_hpf,
+                          "/g/schwab/marco/projects/tem_classification/cryo_hpf_alluvial.png")
+
+
+
+
+
+
+make_cryo_alluvial_figure(df=df_pf, 
+                          response_colors=rc_pf, 
+                          survey_colors=sc_pf,
+                          "/g/schwab/marco/projects/tem_classification/cryo_pf_alluvial.png")
+
+
+
+
+
+
+make_cryo_alluvial_figure(df=df_wm, 
+                          response_colors=rc_wm, 
+                          survey_colors=sc_wm,
+                          "/g/schwab/marco/projects/tem_classification/cryo_wm_alluvial.png")
+
+
+
+
+
+# 
+# 
+# df <- df_pf
+# response_colors <- rc_pf
+# survey_colors <- sc_pf
+make_cryo_alluvial_figure <- function(df, response_colors, survey_colors, outpath){
+
+  
+response_colors <- response_colors %>% filter(response %in% df$response)  
 
 survey_count <- df %>%
   filter(!is.na(response)) %>%
@@ -78,7 +198,7 @@ response_count <- df %>%
   tally() %>%
   mutate(
     angle=ifelse(survey=="Site", 90, 270),
-    hjust=ifelse(survey=="Site", -2, 5),
+    #hjust=ifelse(survey=="Site", -2, 5),
     response=factor(response, levels=response_colors$response)
   ) %>%
   arrange(response)
@@ -121,10 +241,6 @@ response_count_angles <- setNames(
   response_count$response
 )
 
-response_count_hjust <- setNames(
-  response_count$hjust,
-  response_count$response
-)
 
 p <- ggplot(
   pd,
@@ -163,7 +279,7 @@ p <- ggplot(
   geom_text(
     data=survey_count,
     inherit.aes=F,
-    aes(x=survey, y=max_in_figure+20, label=top_lab),
+    aes(x=survey, y=max_in_figure+(max_in_figure/40), label=top_lab),
     vjust=0
   )+
   ## count per tile (on the side)
@@ -187,12 +303,21 @@ p <- ggplot(
     legend.position = "none"
   )
 
+
 png(
-  filename = "my_figure.png",
+  filename = outpath,
   width = 2100,
   height = 1400,
   res = 300
 )
-p
+print(p)
 dev.off()
+print(paste("stored at:", outpath))
+
+return(p)
+
+}
+
+
+
 
